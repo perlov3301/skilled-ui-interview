579acfe1a1a8099b8e94bb5ea42d3cbbfcee9709
Alexander Perlov <alexanderperlov@Alexanders-MacBook-Air.local>
2020-08-13 07:25:51 +0300

    Updates

---
+++ precache-manifest.1a03297e2d71be3d2b3195f8ddec3d64.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d84cce952ecb066f39545fcf499471a",
+    "url": "/perlov3301/skilled-ui-interview/index.html"
+  },
+  {
+    "revision": "ddc09d7308850b286cef",
+    "url": "/perlov3301/skilled-ui-interview/static/css/main.7726becb.chunk.css"
+  },
+  {
+    "revision": "e7698db5a39770dd4920",
+    "url": "/perlov3301/skilled-ui-interview/static/js/2.41759f4c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/perlov3301/skilled-ui-interview/static/js/2.41759f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc09d7308850b286cef",
+    "url": "/perlov3301/skilled-ui-interview/static/js/main.9983ad4e.chunk.js"
+  },
+  {
+    "revision": "28ce61a08bcb211fc581",
+    "url": "/perlov3301/skilled-ui-interview/static/js/runtime-main.713dcf0f.js"
+  },
+  {
+    "revision": "6a9cf1e70c7db68c1d05be37c4b1ca67",
+    "url": "/perlov3301/skilled-ui-interview/static/media/Gauguin_Gogh.6a9cf1e7.jpg"
+  },
+  {
+    "revision": "dc0eada64f8b46f7c4ad3c63252996bc",
+    "url": "/perlov3301/skilled-ui-interview/static/media/my200801.dc0eada6.jpeg"
+  },
+  {
+    "revision": "b1e1311d7451b253625aceaf0105a73e",
+    "url": "/perlov3301/skilled-ui-interview/static/media/nike1.b1e1311d.jpg"
+  },
+  {
+    "revision": "f9e66358ed2b0ef2b69d0187b73c9fd9",
+    "url": "/perlov3301/skilled-ui-interview/static/media/nike2.f9e66358.jpg"
+  }
+]);
